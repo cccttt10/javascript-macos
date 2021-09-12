f0020d2541a4768df5b4a5bd8e53ec24fef6a944
Chuntong Gao <heihegao@gmail.com>
2021-09-12 00:38:15 -0700

    update build script in package.json & update base url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh()],
-    base: '/javascript0macos/',
+    base: '/javascript-macos/',
 });
