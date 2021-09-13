1bf543fd35c7f177dced1710ff51e28e9bfa73e8
Chuntong Gao <heihegao@gmail.com>
2021-09-12 17:59:17 -0700

    add Main componennt

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import './index.scss';
+
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className="AppMain">
+            <div id="main-view" />
+        </div>
+    );
+};
+
+export default Main;
