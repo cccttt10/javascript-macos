8a4ba109fe629a310c1bdfbe0d8565da9140a271
Chuntong Gao <heihegao@gmail.com>
2021-10-17 18:29:53 -0700

    Updates

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    './src/**/*.{ts,tsx}': [
-        'prettier --config ./.prettierrc.json --check',
-        'eslint --no-error-on-unmatched-pattern'
-    ],
-    './src/**/*.{css,scss}': [
-        'stylelint --fix'
-    ]
-};
